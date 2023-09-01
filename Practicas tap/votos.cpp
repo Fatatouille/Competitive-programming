60fda34a9693c3a9d31a8016ed492dfdaf88f507
Fatatouille <108636498+Fatatouille@users.noreply.github.com>
2023-09-01 12:06:37 -0300

    Add files via upload

---
+++ Practicas tap/votos.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    int N, v[10], a, maximo1=0, maximo2=0,total = 0;
+    cin >> N;
+    double porcentaje1, porcentaje2;
+    for (int i=0; i<N; i++){
+        cin >> a;
+        v[i] = a;
+        total= total+a;
+        if (v[i]>maximo1){
+            maximo2=maximo1;
+            maximo1=v[i];
+        }else if(v[i]>maximo2){
+            maximo2=v[i];
+        }
+    }
+    porcentaje1=(maximo1/total)*100;
+    porcentaje2=(maximo2/total)*100;
+    if(porcentaje1>45){
+        a=1;
+    }else if(porcentaje1-porcentaje2 >= 10){
+        a=1;
+    }else{
+        a=2;
+    }
+    cout << a;
+    return 0;
+}
