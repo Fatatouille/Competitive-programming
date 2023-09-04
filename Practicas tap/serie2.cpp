66ac30ada6adb9406e368dbcf31c8831dd2d71a3
Fatatouille <108636498+Fatatouille@users.noreply.github.com>
2023-09-04 06:47:47 -0300

    Add files via upload

---
+++ Practicas tap/serie2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int N=1, M=1,suma, i, anterior, a, b;
+    string C, num[100];
+    while (N>0 && M>0){
+        cin >> N >> M;
+        if(N==-1 && M==-1){
+            break;
+        }
+        cin.ignore();
+        getline(cin, C);
+        suma=0;
+        a=0;
+        for (i=0; i<C.size(); i++){
+            num[a]+=C[i];
+            if (C[i]==' ' || i==(C.size()-1)){
+                anterior=0;
+                if(suma==0){
+                    anterior=0;
+                }else{
+                    for(b=a-1; b!=-1; b--){
+                        anterior+=stoi(num[b])*M;
+                    }
+                }
+                suma+=(stoi(num[a])*M)+anterior;
+                a++;
+            }
+        }
+        cout << suma << endl;
+        for (a=a; a!=-1; a--){
+            num[a]=" ";
+        }
+    }
+    return 0;
+}
